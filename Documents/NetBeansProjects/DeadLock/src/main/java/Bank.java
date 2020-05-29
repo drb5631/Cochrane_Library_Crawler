e2dcc5b683074ca0c47a081b827c2813475d12b3
david <davidblonjr@gmail.com>
2020-05-29 19:33:41 -0400

    Added synchronized doe bank methods to cause deadlock

--- Documents/NetBeansProjects/DeadLock/src/main/java/Bank.java
+++ Documents/NetBeansProjects/DeadLock/src/main/java/Bank.java
@@ -10,16 +10,16 @@ public class Bank {
         Arrays.fill(accounts, initialBalance);
      }
 
-     public void transfer(int from, int to, double amount) {
+     public synchronized void transfer(int from, int to, double amount) {
         if (accounts[from] < amount) return;
         System.out.print(Thread.currentThread());
         accounts[from] -= amount;
         System.out.printf(" %10.2f from %d to %d", amount, from, to);
-       accounts[to] += amount;
+        accounts[to] += amount;
         System.out.printf(" Total Balance: %10.2f%n", getTotalBalance());
      }
 
-     public double getTotalBalance() {
+     public synchronized double getTotalBalance() {
         double sum = 0;
 
         for (double a : accounts)
