815d38b2d4bc863d9fdde48cbb1410623e605452
david <davidblonjr@gmail.com>
2020-05-29 20:12:21 -0400

    Added locks for deadlock functionality

--- Documents/NetBeansProjects/DeadLock/src/main/java/Bank.java
+++ Documents/NetBeansProjects/DeadLock/src/main/java/Bank.java
@@ -1,17 +1,31 @@
 
 import java.util.Arrays;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Bank {
     
      private final double[] accounts;
+     private Lock bankLock = new ReentrantLock();
+     private Condition sufficientFunds = bankLock.newCondition();
 
      public Bank(int n, double initialBalance) {
         accounts = new double[n];
         Arrays.fill(accounts, initialBalance);
      }
 
-     public synchronized void transfer(int from, int to, double amount) {
-        if (accounts[from] < amount) return;
+     public void transfer(int from, int to, double amount) throws InterruptedException {
+        bankLock.lock();
+        try {
+            while (accounts[from] < amount) {
+                sufficientFunds.await();
+            }
+        }
+        finally {
+            bankLock.unlock();
+        }
+        
         System.out.print(Thread.currentThread());
         accounts[from] -= amount;
         System.out.printf(" %10.2f from %d to %d", amount, from, to);
@@ -19,13 +33,19 @@ public class Bank {
         System.out.printf(" Total Balance: %10.2f%n", getTotalBalance());
      }
 
-     public synchronized double getTotalBalance() {
-        double sum = 0;
+     public double getTotalBalance() {
+         bankLock.lock();
+         try {
+            double sum = 0;
 
-        for (double a : accounts)
-           sum += a;
+            for (double a : accounts)
+            sum += a;
 
-        return sum;
+            return sum;
+        }
+         finally {
+             bankLock.unlock();
+         }
      }
 
      public int size() {
