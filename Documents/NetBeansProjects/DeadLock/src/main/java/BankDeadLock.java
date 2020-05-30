8e69fd90683fa5224e7fa2c128397b6147311430
david <davidblonjr@gmail.com>
2020-05-29 20:19:08 -0400

    More small changes for deadlock functionality

--- Documents/NetBeansProjects/DeadLock/src/main/java/BankDeadLock.java
+++ Documents/NetBeansProjects/DeadLock/src/main/java/BankDeadLock.java
@@ -5,20 +5,16 @@ public class BankDeadLock {
      public static final double MAX_AMOUNT = 1000;
      public static final int DELAY = 10;
 
-     public static void main(String[] args)
-     {
+     public static void main(String[] args) {
         Bank bank = new Bank(NACCOUNTS, INITIAL_BALANCE);
         for (int i = 0; i < NACCOUNTS; i++) {
            int fromAccount = i;
            Runnable r = () -> {
-              try
-              {
-                 while (true)
-                 {
+              try {
+                 while (true) {
                     int toAccount = (int) (bank.size() * Math.random());
                     double amount = MAX_AMOUNT * Math.random();
-                    bank.transfer(fromAccount, toAccount, amount);
-                    Thread.sleep((int) (DELAY * Math.random()));
+                    bank.transfer(toAccount, fromAccount, amount);
                  }
               }
               catch (InterruptedException e) {
