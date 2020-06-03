ee26e1eee43103e6f1903f3baea1a373a98c2092
david <davidblonjr@gmail.com>
2020-06-03 15:09:18 -0400

    New Lab Start

---
+++ Documents/IST 411/ConnectionsLab/src/main/java/ConnectionsLab/Noaa.java
@@ -0,0 +1,35 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Noaa {
+    
+    public Noaa() {
+        String acccessToken = "hsgVQCUJhguxzaGCZNAqfMqYcZiXcKvA";
+        Gson gson = new Gson();
+        String response;
+        HttpURLConnection connection = null;
+        
+        try {
+            URL url = new URL("https://www.ncdc.noaa.gov/cdo-web/access/services/data/v1");
+            connection = (HttpURLConnection) url.openConnection();
+            connection.getRequestProperty("token", acccessToken);
+            BufferedReader br = new BufferedReader(
+                                new InputStreamReader(connection.getInputStream()));
+            response = br.readLine();
+            System.out.println(response + "\n");
+        }
+        catch (IOException e) {
+            
+            System.out.println(e);
+        }
+        finally {
+            connection.disconnect();
+        }
+    }
+    
+}
