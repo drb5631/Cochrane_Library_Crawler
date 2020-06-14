57eb12ebc382824aeb648897751471cf9c543804
ruc230 <ruc230@psu.edu>
2020-06-14 14:16:49 -0400

    Fixed the path to config file to be relative

--- Documents/NetBeansProjects/Callisto_Financial_Application/src/main/java/Util/CredentialCheck.java
+++ Documents/NetBeansProjects/Callisto_Financial_Application/src/main/java/Util/CredentialCheck.java
@@ -4,24 +4,25 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  *
  * @author david
  */
 public class CredentialCheck {
+    String basePathString = Paths.get("").toAbsolutePath().toString();
+    private final String README = basePathString + "\\src\\main\\java\\Demo\\README.txt";
 
-    private final String README = "C:\\Users\\david\\Documents\\NetBeansProjects\\Callisto_Financial_Application\\src\\main\\java\\Demo\\README.txt";
-
-    public boolean verifyCredentials(String username, String password) {
-
+    public boolean verifyCredentials(String username, String password) {   
         BufferedReader bufferedReader;
         boolean verifiedCredentials = false;
-        
+
         try {
             bufferedReader = new BufferedReader(new FileReader(README));
             String line;
-            while ((line = bufferedReader.readLine()) != null) {
+            while ((line = bufferedReader.readLine().trim()) != null) {
                 if (line.equals(username + ", " + password)) {
                     verifiedCredentials = true;
                     break;
